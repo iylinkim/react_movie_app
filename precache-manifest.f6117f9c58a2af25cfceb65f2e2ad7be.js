88213413015dee47443fda2f875fd3aa0cf7700f
Iylin <kiylin94@naver.com>
2020-10-17 12:38:13 +0900

    Updates

---
+++ precache-manifest.f6117f9c58a2af25cfceb65f2e2ad7be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2d7c50d212492d50b996313ec3121ff",
+    "url": "/react_movie_app/index.html"
+  },
+  {
+    "revision": "15a8cdb3c71fb9090ee9",
+    "url": "/react_movie_app/static/css/main.20ff916f.chunk.css"
+  },
+  {
+    "revision": "68e91e310ee8cdc91a32",
+    "url": "/react_movie_app/static/js/2.7207c08f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_movie_app/static/js/2.7207c08f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15a8cdb3c71fb9090ee9",
+    "url": "/react_movie_app/static/js/main.2ff6e18b.chunk.js"
+  },
+  {
+    "revision": "b2eab6cde90deae0fa53",
+    "url": "/react_movie_app/static/js/runtime-main.e749ff8d.js"
+  },
+  {
+    "revision": "0ba63dfce371d8b9a50876e76d0a214a",
+    "url": "/react_movie_app/static/media/ProximaNova-Regular.0ba63dfc.woff"
+  }
+]);
